273ae76c07357a3a006c06fa39473865d8ba826c
Anders Einar Hilden <hildenae@gmail.com>
2018-01-29 17:08:24 +0000

    use find, not for

--- extract.sh
+++ extract.sh
@@ -4,6 +4,11 @@ SOURCE_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 cd "${SOURCE_DIR}"
 
 source config.sh
+#STORAGE_PATH="$PWD/data"
+#THREADS=3
+#MCN_TOOLS="/root/mcn-tools"
+#DOMAINS="mcn-source-wayback.list"
+
 TMP="./extraxt.tmp"
 
 function ext {
@@ -16,11 +21,17 @@ function ext {
         continue;
     fi
     echo "${X} extracted ok";
+    jq -r '.[][2]' "${TMP}" | uniq 
     #while [ $(df . | awk '/vda1/{print substr($5, 1, length($5)-1)}') -gt 52 ]; do echo "less that 52";sleep 10s; done
   done
 }
 
 ext "$(cat errors.txt| sort -R | head -1 | grep -o '/root.*.gz')"
 ext "/root/mcn-source-wayback/data/domain-no-40000.gz"
+
+find ${STORAGE_PATH}/ -type f -name '*.gz' -exec gunzip -c {} \; | \
+        parallel --jobs ${THREADS} --pipe iconv -c -f utf-8 -t utf-8 | \
+        sed -e 's/\.no\.html/.html/g' | \
+        ${MCN_TOOLS}/urldecode 3 | ${MCN_TOOLS}/default_extract > "${DOMAINS}"
 
 rm "${TMP}"
