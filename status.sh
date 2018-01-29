6ea517a382505037920b205c1b1cb3c01b1dcd15
Anders Einar Hilden <hildenae@gmail.com>
2018-01-29 16:55:38 +0000

    Slightly more readable status.sh

--- status.sh
+++ status.sh
@@ -1,2 +1,9 @@
 #! /bin/zsh
-echo -n "Errors: "; A=$(for X in data/*; do zcat $X | tail -1; done | grep -E '\]\]|\[\]' | wc -l) 2>&1 | grep -c 'unexpected end'; B=$(ls data | wc -l); echo -e "OK:$A\nTotal:$B"
+cd "${0:a:h}"
+source config.sh
+
+echo -n "Errors: ";
+A=$(for X in ${STORAGE_PATH}/*; do
+	zcat $X | tail -1; done | grep -E '\]\]|\[\]' | wc -l) 2>&1 | grep 'unexpected end';
+B=$(ls data | wc -l);
+echo -e "OK:$A\nTotal:$B"
