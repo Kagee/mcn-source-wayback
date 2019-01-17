ed019775fa7b6fd6496acabdea4f04f08b0dbca4
Anders Einar Hilden <hildenae@gmail.com>
2019-01-17 11:27:49 +0100

    test functions

--- list_domains.sh
+++ list_domains.sh
@@ -1,19 +1,26 @@
 #! /bin/bash
 SOURCE_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
-# This way we can work with realtive paths in the rest of the script
 cd "${SOURCE_DIR}"
 
 source config.sh
 #STORAGE_PATH="$PWD/data"
 #THREADS=3
-#MCN_TOOLS="/root/mcn-tools"
 #DOMAINS="mcn-source-wayback.list"
+YMD="$(date +%F)"
+function test_storage_space {
+  if [ $(df "$STORAGE_PATH" | awk '/\/dev/{print substr($5, 1, length($5)-1)}') -gt $1 ]; then
+    pushover "Storage space less than $1%, halting wayback extraction"
+    while [ $(df "$STORAGE_PATH" | awk '/\/dev/{print substr($5, 1, length($5)-1)}') -gt $1 ]; do
+        echo "more than $1% storage on drive of \$STORAGE_PATH used";
+        sleep 5m;
+    done
+  fi
+}
 
 TMP="./extract.tmp"
 TMP2="./extract2.tmp"
-
+mkdir data_urlz
 find ${STORAGE_PATH}/ -type f -name '*.gz' | sort | while read X; do
-  #echo "${X}"
   OUT="$(dirname "${X}")_urlz/$(basename "${X}" .gz).urlz"
   if [ ! -f "${OUT}" ]; then 
     gunzip -c "$X" 2> /dev/null > "${TMP}"
@@ -26,21 +33,15 @@ find ${STORAGE_PATH}/ -type f -name '*.gz' | sort | while read X; do
         iconv -c -f utf-8 -t utf-8 | sed -e 's/\.no\.html/.html/g' | \
         ${MCN_TOOLS}/urldecode 3 | gzip > "${TMP2}";
     mv "${TMP2}" "${OUT}";
-    if [ $(df . | awk '/vda1/{print substr($5, 1, length($5)-1)}') -gt 90 ]; then
-      pushover "Storage space less than 90%, halting url extraction"
-      while [ $(df . | awk '/vda1/{print substr($5, 1, length($5)-1)}') -gt 90 ]; do 
-          echo "more than 90% of /dev/vda1/ used";
-          sleep 5m; 
-      done
-    fi
+    test_storage_space 94
   else
     echo "Found ${OUT}, skipping";
   fi
 done
-
-#find ${STORAGE_PATH}/ -type f -name '*.gz' -exec gunzip -c {} \; | \
-#        parallel --jobs ${THREADS} --pipe iconv -c -f utf-8 -t utf-8 | \
-#        sed -e 's/\.no\.html/.html/g' | \
-#        ${MCN_TOOLS}/urldecode 3 | ${MCN_TOOLS}/default_extract > "${DOMAINS}"
-
-rm "${TMP}" "${TMP2}"
+mv output/* old/
+find ${STORAGE_PATH}_urlz/ -type f -name '*.urlz' -exec gunzip -c {} \; | \
+        parallel --jobs ${THREADS} --pipe iconv -c -f utf-8 -t utf-8 | \
+        sed -e 's/\.no\.html/.html/g' | \
+        ${MCN_TOOLS}/urldecode 3 | ${MCN_TOOLS}/default_extract > "${DOMAINS}"
+mv "${DOMAINS}" "output/${YMD}-${DOMAINS}"
+rm "${TMP}"
